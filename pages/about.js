22da813f195d4adf6356eab2e90ccdd36661abc2
Danli Denny Deng <danlideng@yahoo.com>
2022-05-28 01:13:36 -0700

    bootstrap added

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+function About() {
+  return (
+  <>
+    <button className='btn btn-primary'>Primary</button>
+    <button className='btn btn-success'>Sucess</button>
+
+  </>
+  
+  )
+}
+
+export default About
